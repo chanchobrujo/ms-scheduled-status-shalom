79f87e473efeaec2eb2afda0bcae1558e909ae21
Chancho Brujo <85182515+chanchobrujo@users.noreply.github.com>
2024-11-20 08:49:28 -0500

    REFACTOR: Project init.

---
+++ src/main/java/com/shalom/scheduled_status/ScheduledStatusApplication.java
@@ -0,0 +1,13 @@
+package com.shalom.scheduled_status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shalom/scheduled_status/ScheduledStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalom.scheduled_status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
